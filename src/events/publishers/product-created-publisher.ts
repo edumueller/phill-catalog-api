df65cffb0661cb6cbfdfb8a5368cc9dab2287a74
edumueller <eduardo.nicastro@icloud.com>
2021-08-29 17:33:41 -0300

    fix: typo on prod created publisher file

--- src/events/publishers/product-created-publisher.ts
+++ src/events/publishers/product-created-publisher.ts
@@ -1,5 +1,5 @@
 import { Publisher, Subjects, ProductCreatedEvent } from '@phill-sdk/common';
 
-export class TicketCreatedPublisher extends Publisher<ProductCreatedEvent> {
+export class ProductCreatedPublisher extends Publisher<ProductCreatedEvent> {
   subject: Subjects.ProductCreated = Subjects.ProductCreated;
 }
